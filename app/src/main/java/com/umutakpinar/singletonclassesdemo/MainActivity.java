05a4b750d093df4f2bb1843c344469117123c36a
Umut Akpinar <umtakpnr5@gmail.com>
2022-08-10 12:30:51 +0300

    How to use singleton?

--- app/src/main/java/com/umutakpinar/singletonclassesdemo/MainActivity.java
+++ app/src/main/java/com/umutakpinar/singletonclassesdemo/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String veri = "Lorem ipsum dolor sit amet.";
+        /* Singleton singleton = new Singleton() diyemezsin çünkü cosntructor private */
+        Singleton singleton = Singleton.getInstance(); //Diyerek 1 kere newlenmesini sağlıyoruz bunu tekrar yazsam da newlenemez!
+        singleton.setTasinacakVeri(veri);
+        /*Aynı işlemi başka classta da yaparak bu sefer getTasinacakVeri'yi kullanarak alabilrim...*/
     }
 }
